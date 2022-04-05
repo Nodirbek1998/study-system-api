4fa7376539c772d18e135fa91d027aa235a49e9a
Nodirbek <nodirbekmamadaliyev1998@gmaiil.com>
2022-04-05 12:10:13 +0500

    Study system

--- src/main/java/uz/tatu/service/mapper/ArticleMapper.java
+++ src/main/java/uz/tatu/service/mapper/ArticleMapper.java
@@ -7,10 +7,25 @@ import uz.tatu.service.dto.ArticleDTO;
 /**
  * Mapper for the entity {@link Article} and its DTO {@link ArticleDTO}.
  */
-@Mapper(componentModel = "spring", uses = { StudyUsersMapper.class })
+@Mapper(componentModel = "spring", uses = {UserMapper.class})
 public interface ArticleMapper extends EntityMapper<ArticleDTO, Article> {
-    @Mapping(target = "studyUser", source = "studyUser", qualifiedByName = "id")
-    @Mapping(target = "createdBy", source = "createdBy", qualifiedByName = "id")
-    @Mapping(target = "updatedBy", source = "updatedBy", qualifiedByName = "id")
+
+    @Mapping(source = "users.id", target = "usersId")
+    @Mapping(source = "createdBy.id", target = "createdById")
+    @Mapping(source = "updatedBy.id", target = "updatedById")
     ArticleDTO toDto(Article s);
+
+    @Mapping(source = "usersId", target = "users")
+    @Mapping(source = "createdById", target = "createdBy")
+    @Mapping(source = "updatedById", target = "updatedBy")
+    Article toEntity(ArticleDTO s);
+
+    default Article fromId(Long id) {
+        if (id == null) {
+            return null;
+        }
+        Article article = new Article();
+        article.setId(id);
+        return article;
+    }
 }
