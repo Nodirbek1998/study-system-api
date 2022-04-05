3d39bc3b0e420444450dd9dba9be89bd8e3094cd
Nodirbek <nodirbekmamadaliyev1998@gmaiil.com>
2022-04-05 12:06:38 +0500

    Study system

--- src/main/java/uz/tatu/service/dto/FilesDTO.java
+++ src/main/java/uz/tatu/service/dto/FilesDTO.java
@@ -1,5 +1,9 @@
 package uz.tatu.service.dto;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
@@ -7,6 +11,10 @@ import java.util.Objects;
 /**
  * A DTO for the {@link uz.tatu.domain.Files} entity.
  */
+
+@Data
+@EqualsAndHashCode
+@ToString
 public class FilesDTO implements Serializable {
 
     private Long id;
@@ -19,87 +27,6 @@ public class FilesDTO implements Serializable {
 
     private LocalDate createdAt;
 
-    private StudyUsersDTO createdBy;
+    private Long createdById;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Double getFileSize() {
-        return fileSize;
-    }
-
-    public void setFileSize(Double fileSize) {
-        this.fileSize = fileSize;
-    }
-
-    public String getContentType() {
-        return contentType;
-    }
-
-    public void setContentType(String contentType) {
-        this.contentType = contentType;
-    }
-
-    public LocalDate getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDate createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public StudyUsersDTO getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(StudyUsersDTO createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof FilesDTO)) {
-            return false;
-        }
-
-        FilesDTO filesDTO = (FilesDTO) o;
-        if (this.id == null) {
-            return false;
-        }
-        return Objects.equals(this.id, filesDTO.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(this.id);
-    }
-
-    // prettier-ignore
-    @Override
-    public String toString() {
-        return "FilesDTO{" +
-            "id=" + getId() +
-            ", name='" + getName() + "'" +
-            ", fileSize=" + getFileSize() +
-            ", contentType='" + getContentType() + "'" +
-            ", createdAt='" + getCreatedAt() + "'" +
-            ", createdBy=" + getCreatedBy() +
-            "}";
-    }
 }
--- src/main/java/uz/tatu/service/mapper/FilesMapper.java
+++ src/main/java/uz/tatu/service/mapper/FilesMapper.java
@@ -7,8 +7,21 @@ import uz.tatu.service.dto.FilesDTO;
 /**
  * Mapper for the entity {@link Files} and its DTO {@link FilesDTO}.
  */
-@Mapper(componentModel = "spring", uses = { StudyUsersMapper.class })
+@Mapper(componentModel = "spring", uses = { UserMapper.class })
 public interface FilesMapper extends EntityMapper<FilesDTO, Files> {
-    @Mapping(target = "createdBy", source = "createdBy", qualifiedByName = "id")
+
+    @Mapping(source = "createdBy.id", target = "createdById")
     FilesDTO toDto(Files s);
+
+    @Mapping(source = "createdById", target = "createdBy")
+    Files toDto(FilesDTO s);
+
+    default Files fromId(Long id) {
+        if (id == null) {
+            return null;
+        }
+        Files files = new Files();
+        files.setId(id);
+        return files;
+    }
 }
